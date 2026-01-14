74589f1a665790a0fdbeb3c98386c60f002fabbc
Weilin (Alan) Wu <wwu@ccia.org.au>
2026-01-14 22:10:41 +1100

    added class validation for validating update and create user input, added logger for production and dev envrionment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useGlobalPipes(new ValidationPipe()); // add a pipe to process all incomming requests before hitting the routes
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.getOrThrow('PORT'));
 }
 bootstrap();
